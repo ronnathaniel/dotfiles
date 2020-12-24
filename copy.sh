ef40f05386936c520907fe4b1c829dafaa72aa9d
Ron Nathaniel <rnathaniel7@gmail.com>
2020-12-23 22:43:25 -0500

    updating finding to be more precise

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 
 shopt -s nullglob
-for i in `find . -type f -name ".*"`;
+for i in `find . -type f -name ".*" -and ! -name "*git*" -and ! -name "*.vim/*"`;
 do
   cp $i ~/.
   echo copied $i to ~/$i
