169a2e5abf261fe41a64ddfc15c20d6213f761f1
ronnathaniel <rnathaniel7@gmail.com>
2021-10-05 18:48:04 -0400

    fix(copy): pulls submodule + copies all .vim/

--- copy.sh
+++ copy.sh
@@ -1,7 +1,16 @@
 
+git pull --recurse-submodules
+git submodule update --recursive --remote
+
+
 shopt -s nullglob
-for i in `find . -type f -name ".*" -and ! -name "*git*" -and ! -name "*.vim/*"`;
+for i in `find . -type f -name ".*" -and ! -name "*git*"`;
 do
+  # -type f
+  # -and ! -name "*.vim/*"
   cp $i ~/.
   echo copied $i to ~/$i
 done
+
+cp -r ./.vim ~
+echo copied .vim to ~/.vim
